7188e6bfa1e065c6a113d2ebbdf7efe563c71f10
Johannes Goebel <GoebelJohannes@gmx.net>
2019-01-08 18:58:39 +0100

    - fixed click issue

--- src/main/java/com/github/appreciated/ClickablePaperCard.java
+++ src/main/java/com/github/appreciated/ClickablePaperCard.java
@@ -6,11 +6,12 @@ import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.shared.Registration;
 import com.vaadin.flow.templatemodel.TemplateModel;
 
 @Tag("clickable-paper-card")
 @HtmlImport("frontend://com/github/appreciated/paper-card/clickable-paper-card.html")
-public class ClickablePaperCard extends PolymerTemplate<TemplateModel> implements HasComponents, FlexComponent, ClickNotifier<ClickablePaperCard> {
+public class ClickablePaperCard extends PolymerTemplate<TemplateModel> implements HasComponents, FlexComponent, ClickNotifier {
     @Id("card")
     private PaperCard card;
 
@@ -36,15 +37,24 @@ public class ClickablePaperCard extends PolymerTemplate<TemplateModel> implement
         card.add(ripple);
     }
 
-    public ClickablePaperCard(Component content, ComponentEventListener<ClickEvent<ClickablePaperCard>> listener) {
+    public ClickablePaperCard(Component content, ComponentEventListener listener) {
         this(content, (Component[]) null);
         addClickListener(listener);
     }
 
-    public ClickablePaperCard(Component content, ComponentEventListener<ClickEvent<ClickablePaperCard>> listener, Component... actions) {
+    public ClickablePaperCard(Component content, ComponentEventListener listener, Component... actions) {
         this(content, actions);
         addClickListener(listener);
     }
+
+    public Registration addClickListener(ComponentEventListener listener) {
+        if (this instanceof Component) {
+            return ComponentUtil.addListener(card.getContent(), ClickEvent.class, listener);
+        } else {
+            throw new IllegalStateException(String.format("The class '%s' doesn't extend '%s'. Make your implementation for the method '%s'.", this.getClass().getName(), Component.class.getSimpleName(), "addClickListener"));
+        }
+    }
+
 
     @Override
     public void add(Component... components) {
